95bebefb2226794a0b5a6b3fb51d1d33f94f8a8b
GulshanJha00 <gk4763549@gmail.com>
2026-03-13 00:12:23 +0530

    checkout added

--- main.cpp
+++ main.cpp
@@ -12,6 +12,11 @@ void init()
 {
     cout << "Initializing " << endl;
     string folderName = ".miniVCS";
+    if (fs::exists(folderName))
+    {
+        cout << "Already initialized " << endl;
+        return;
+    }
 
     if (mkdir(folderName.c_str(), 0777) == 0)
     {
@@ -52,44 +57,65 @@ void init()
 
 void add()
 {
-    cout << "Choose file to be added:- ";
-    string filePath;
-    cin >> filePath;
-    ifstream inFile(filePath, ios::binary);
-
-    if (!inFile)
+    while (true)
     {
-        cout << "Failed to open file. Make sure it exists!" << endl;
+        cout << "Type 'exit' to exit" << endl;
+        cout << "Choose file to be added:- ";
+        string filePath;
+        cin >> filePath;
+        if (filePath == "exit")
+        {
+            cout << "Exited Successful" << endl;
+            return;
+        }
+        if (!fs::exists(filePath))
+        {
+            cout << "File does not exit " << endl;
+            continue;
+        }
+
+        ifstream inFile(filePath, ios::binary);
+
+        if (!inFile)
+        {
+            cout << "Failed to open file. Make sure it exists!" << endl;
+        }
+
+        string line;
+        string newPath = ".miniVCS/index/" + filePath;
+        ofstream Myfile(newPath, ios::binary);
+        Myfile << inFile.rdbuf();
+
+        inFile.close();
+        cout << "File Added Successful";
     }
-
-    string line;
-    string newPath = ".miniVCS/index/" + filePath;
-    ofstream Myfile(newPath, ios::binary);
-    Myfile << inFile.rdbuf();
-
-    inFile.close();
-    cout << "File Added Successful";
 }
 
 void commit()
 {
+    string folderPath = ".miniVCS/index/";
+    if (fs::is_empty(folderPath))
+    {
+        cout << "Nothing to commit " << endl;
+        return;
+    }
     cout << "Add Commit Number:- ";
     string commitId;
     cin >> commitId;
 
-    string folderPath = ".miniVCS/index/";
     string commitFolder = ".miniVCS/commits/" + commitId;
+    if (fs::exists(commitFolder))
+    {
+        cout << "Commit number already exits " << endl;
+        return;
+    }
 
     fs::create_directories(commitFolder);
 
-    if(fs::is_empty(folderPath)){
-        cout<<"Nothing to commit "<<endl;
-        return;
-    }
     for (const auto &entry : fs::directory_iterator(folderPath))
     {
-        string fileName = entry.path().filename();
-        ofstream Myfile(commitFolder + "/" + fileName);
+        string fileName = entry.path().filename().string();
+        ofstream Myfile(commitFolder + "/" + fileName,ios::binary);
         string sourcePath = folderPath + fileName;
         ifstream inFile(sourcePath, ios::binary);
         Myfile << inFile.rdbuf();
@@ -97,12 +123,49 @@ void commit()
         Myfile.close();
         fs::remove(folderPath + fileName);
     }
-    cout<<"Committed Successfully "<<endl;
+    cout << "Committed Successfully " << endl;
 }
+
 void checkout()
 {
-    cout << "Checking out ";
+    string indexFile = ".miniVCS/index";
+
+    if (!fs::exists(indexFile))
+    {
+        cout << "Index folder missing\n";
+        cout << "Please Initialize before checkout\n";
+        return;
+    }
+
+    if (!fs::is_empty(indexFile))
+    {
+        cout << "Please Commit Unsaved Files Before Checkout.\n";
+        return;
+    }
+
+    string commitName;
+    cout << "Enter commit Checkout:- ";
+    cin >> commitName;
+
+    string commitsLocation = ".miniVCS/commits/" + commitName;
+    if(!fs::exists(commitsLocation)){
+        cout<<"Commit number does not exit "<<endl;
+        return;
+    }
+
+
+    for (auto const &entry : fs::directory_iterator(commitsLocation))
+    {
+        string sourceFile = entry.path().string();
+        string fileName = entry.path().filename().string();
+
+        ifstream src(sourceFile, ios::binary);
+        ofstream dst(fileName, ios::binary);
+        dst << src.rdbuf();
+    }
+    cout << " Checkout Completed " << endl;
 }
+
 int main()
 {
     string a;
